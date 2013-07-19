437872cf07e58542d1272097ae541fb6ccb375c1
Fernandolima <fernandolima66@gmail.com>
2013-07-19 10:38:46 -0700

    Fernando Lindo

--- WebHome/src/main/java/br/com/webhome/entity/Usuario.java
+++ WebHome/src/main/java/br/com/webhome/entity/Usuario.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import net.sourceforge.jtds.jdbc.DateTime;
+
 @Entity
 // Ela declara a classe como persistente e gerenciada pelo Hibernate
 @Table(name = "dbo.USER")
@@ -19,6 +21,21 @@ public class Usuario implements Serializable {
 	@Column(name = "NOME")
 	private String nome;
 
+	@Column(USUAARIOLOGIN = "LOGIN")
+	private String USUAARIOLOGIN;
+	
+	@Column(email = "EMAIL")
+	private String email;
+	
+	@Column(permissao = "PERMISSAO")
+	private String permissao;
+	
+	@Column(senha = "SENHA")
+	private String senha;
+	
+	@Column(dtNascimento = "DT_NASCIMENTO")
+	private DateTime dtNascimento;
+	
 	@Id
 	// define a propriedade que será o identificador único da classe e da
 	// tabela.
@@ -56,6 +73,76 @@ public class Usuario implements Serializable {
 	 */
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	/**
+	 * @return the uSUAARIOLOGIN
+	 */
+	public String getUSUAARIOLOGIN() {
+		return USUAARIOLOGIN;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @return the permissao
+	 */
+	public String getPermissao() {
+		return permissao;
+	}
+
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
+
+	/**
+	 * @return the dtNascimento
+	 */
+	public DateTime getDtNascimento() {
+		return dtNascimento;
+	}
+
+	/**
+	 * @param uSUAARIOLOGIN the uSUAARIOLOGIN to set
+	 */
+	public void setUSUAARIOLOGIN(String uSUAARIOLOGIN) {
+		USUAARIOLOGIN = uSUAARIOLOGIN;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @param permissao the permissao to set
+	 */
+	public void setPermissao(String permissao) {
+		this.permissao = permissao;
+	}
+
+	/**
+	 * @param senha the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	/**
+	 * @param dtNascimento the dtNascimento to set
+	 */
+	public void setDtNascimento(DateTime dtNascimento) {
+		this.dtNascimento = dtNascimento;
 	}
 }
 // @OneToMany(fetch=FetchType.LAZY, mappedBy="USER")
