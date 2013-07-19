f0eebf14adb0c4bdc97420933777722208eb2c69
Fernandolima <fernandolima66@gmail.com>
2013-07-19 10:16:00 -0700

    COMMITANDO

--- WebHome/src/main/java/br/com/webhome/entity/Usuario.java
+++ WebHome/src/main/java/br/com/webhome/entity/Usuario.java
@@ -2,41 +2,61 @@ package br.com.webhome.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-@Entity //Ela declara a classe como persistente e gerenciada pelo Hibernate
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+// Ela declara a classe como persistente e gerenciada pelo Hibernate
+@Table(name = "dbo.USER")
+// que define qual o nome da tabela no banco de dados ao qual a classe será
+// mapeada
 public class Usuario implements Serializable {
 
-private String nome;
-private Integer id;
-/**
-* @return the nome
-*/
-public String getNome() {
-return nome;
+	@Column(name = "NOME")
+	private String nome;
+
+	@Id
+	// define a propriedade que será o identificador único da classe e da
+	// tabela.
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// É uma boa prática definir uma propriedade id com um valor gerado
+	// automaticamente
+	private Integer id;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param nome
+	 *            the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
 }
-/**
-* @return the id
-*/
-public Integer getId() {
-return id;
-}
-/**
-* @param nome the nome to set
-*/
-public void setNome(String nome) {
-this.nome = nome;
-}
-/**
-* @param id the id to set
-*/
-public void setId(Integer id) {
-this.id = id;
-}
-//@OneToMany(fetch=FetchType.LAZY, mappedBy="USER")
-@Id //define a propriedade que será o identificador único da classe e da tabela.
-@Table(Usuario="dbo.USER")//que define qual o nome da tabela no banco de dados ao qual a classe será mapeada
-@Column(id="ID")
-@Column(nome="NOME")
-@GeneratedValue(strategy=GenerateType.IDENTITY)// É uma boa prática definir uma propriedade id com um valor gerado automaticamente
-@Enumerated(EnumType.ORDINAL) // Ele indica que o valor a ser gravado no banco de dados é um valor numérico correspondente à posição do valor na Enum;
- 
+// @OneToMany(fetch=FetchType.LAZY, mappedBy="USER")
+
