6694ba82acffeed4f36053d60d8e8dd0a594460b
Fernandolima <fernandolima66@gmail.com>
2013-07-19 08:58:06 -0700

    Entity Usuario

--- WebHome/src/main/java/br/com/webhome/entity/Usuario.java
+++ WebHome/src/main/java/br/com/webhome/entity/Usuario.java
@@ -1,16 +1,44 @@
 package br.com.webhome.entity;
 
-import org.hibernate.annotations.Table;
+import java.io.Serializable;
 
-public class Usuario {
+import javax.persistence.Entity;
+@Entity //Ela declara a classe como persistente e gerenciada pelo Hibernate
+public class Usuario implements Serializable {
 	
-	
-	
-	
-	
-	
-	
-	//@Table(dbo.USER)
+	private String nome; 
+	private Integer id;
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	//@OneToMany(fetch=FetchType.LAZY, mappedBy="USER")
+	@Id //define a propriedade que será o identificador único da classe e da tabela.
+	@Table(Usuario="dbo.USER")//que define qual o nome da tabela no banco de dados ao qual a classe será mapeada 
+	@Column(id="ID")
+	@Column(nome="NOME")
+	@GeneratedValue(strategy=GenerateType.IDENTITY)// É uma boa prática definir uma propriedade id com um valor gerado automaticamente
+	@Enumerated(EnumType.ORDINAL) // Ele indica que o valor a ser gravado no banco de dados é um valor numérico correspondente à posição do valor na Enum;
 	
 
 }
