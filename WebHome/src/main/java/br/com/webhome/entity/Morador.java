cfde253cf240e3b866344d4ad0cff79d4914843b
Fernandolima <fernandolima66@gmail.com>
2013-07-24 10:07:08 -0700

    Class criada por orientacao do Diego

--- WebHome/src/main/java/br/com/webhome/entity/Morador.java
+++ WebHome/src/main/java/br/com/webhome/entity/Morador.java
@@ -2,32 +2,13 @@ package br.com.webhome.entity;
 
 import java.sql.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 
 @Entity
-//Ela declara a classe como persistente e gerenciada pelo Hibernate
-@Table(name = "dbo.PUBLICACAO")
-//que define qual o nome da tabela no banco de dados ao qual a classe será
-//mapeada
+
 public class Morador extends Usuario {
 	
-	@Column(name = "PUBLICACAO")
-	private String publicacao;
-	
-	@Id
-	// define a propriedade que será o identificador único da classe e da
-	// tabela.
-	@Column(name = "ID_PUBLICACAO")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	// É uma boa prática definir uma propriedade id com um valor gerado
-	// automaticamente
-	private Integer id_PUBLICACAO;
 	
 	@Override
 	public void setDt_nascimento(Date dt_nascimento) {
@@ -76,33 +57,5 @@ public class Morador extends Usuario {
 		super.setStatus(status);
 	}
 
-	/**
-	 * @return the publicacao
-	 */
-	public String getPublicacao() {
-		return publicacao;
-	}
-
-	/**
-	 * @return the id_PUBLICACAO
-	 */
-	public Integer getId_PUBLICACAO() {
-		return id_PUBLICACAO;
-	}
-
-	/**
-	 * @param publicacao the publicacao to set
-	 */
-	public void setPublicacao(String publicacao) {
-		this.publicacao = publicacao;
-	}
-
-	/**
-	 * @param id_PUBLICACAO the id_PUBLICACAO to set
-	 */
-	public void setId_PUBLICACAO(Integer id_PUBLICACAO) {
-		this.id_PUBLICACAO = id_PUBLICACAO;
-	}
-	
 
 }
