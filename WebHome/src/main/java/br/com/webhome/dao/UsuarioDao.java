f26e78d9d773299c783df5a983f9d30ffe513bf2
Fernandolima <fernandolima66@gmail.com>
2013-07-26 16:06:42 -0700

    UsuarioDao

---
+++ WebHome/src/main/java/br/com/webhome/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+package br.com.webhome.dao;
+
+import java.util.List;
+
+import br.com.webhome.entity.Morador;
+import br.com.webhome.to.MoradorTO;
+
+public class UsuarioDao {
+
+	  public MoradorTO save(Morador morador);
+	   // public Morador getMorador(int id);
+	    public MoradorTO getMorador(String login);
+	    public List<MoradorTO> getMoradores();
+	    public void remove(Morador morador);
+	    public void update(Morador morador);
+	
+}
