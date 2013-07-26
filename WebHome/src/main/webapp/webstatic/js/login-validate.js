812b3385448e982599d84ec2166135e5938bc6d2
tatianedias <tatinhadias@gmail.com>
2013-07-26 16:58:38 -0700

    [TATI] Comentário no JS de login

--- WebHome/src/main/webapp/webstatic/js/login-validate.js
+++ WebHome/src/main/webapp/webstatic/js/login-validate.js
@@ -2,9 +2,11 @@ var LOGIN = {
 	hash: null,
 	
 	init: function() {
+		//pega parâmetro da URL
 		hash = location.href.split('?');
 		hash = hash[1];
 		
+		//faz os tratamentos de erros conforme hash passada por parâmetro
 		if(hash !== '') {
 			switch (hash) {
 				case 'error':
@@ -21,10 +23,12 @@ var LOGIN = {
 }
 
 $(function() {
+	//valida se está na página de login
 	if($('#login-view')[0]){
 		LOGIN.init();
 	}
 	
+	//quando o form for submetido exibe o load e esconde o botão
 	$('#frmLogin').on('submit', function(){
 		$('#btSubmitLogin').css('display', 'none');
 		$('#load-login').css('display', 'block');
