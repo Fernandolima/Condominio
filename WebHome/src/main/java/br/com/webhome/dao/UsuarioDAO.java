da5820cf3244ec41c91b0caf5627e177c2efa060
Fernandolima <fernandolima66@gmail.com>
2013-07-26 16:36:37 -0700

    Tentando pela 10vedz

---
+++ WebHome/src/main/java/br/com/webhome/dao/UsuarioDAO.java
@@ -0,0 +1,18 @@
+package br.com.webhome.dao;
+
+import java.util.List;
+
+import br.com.webhome.entity.Usuario;
+import br.com.webhome.to.UsuarioTO;
+
+public interface UsuarioDAO {
+	
+
+		  public UsuarioTO save(Usuario usuario);
+		   // public Morador getMorador(int id);
+		   // public UsuarioTO getUsurio(String login);
+		    public List<UsuarioTO> getUsuario();
+		  
+	}
+
+
