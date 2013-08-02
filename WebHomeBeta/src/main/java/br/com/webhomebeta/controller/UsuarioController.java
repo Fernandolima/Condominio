4251c19bbe9b48091f4989bbbaf54313aebd3efe
Fernandolima <fernandolima66@gmail.com>
2013-08-02 08:57:23 -0700

    Class Nova :D

--- WebHomeBeta/src/main/java/br/com/webhomebeta/controller/UsuarioController.java
+++ WebHomeBeta/src/main/java/br/com/webhomebeta/controller/UsuarioController.java
@@ -1,10 +1,16 @@
 package br.com.webhomebeta.controller;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.ObjectInputStream.GetField;
+import java.util.Map;
+
+import javassist.expr.NewArray;
+
+import javax.naming.Binding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.webhomebeta.entity.Usuario;
 import br.com.webhomebeta.service.EmailServico;
 import br.com.webhomebeta.service.UsuarioService;
+import br.com.webhomebeta.to.UsuarioTO;
 
 @Controller
 @RequestMapping("/cadastro")
@@ -39,48 +46,12 @@ public class UsuarioController {
 		usuario.setPermissao("ROLE_MORADOR");
 		String email = usuario.getEmail();
 		usuario.setLogin(email);
-		isValidCPF(usuario.getCpf());
+		ValidacoesController validacoesController = new ValidacoesController();
+		validacoesController.isValidCPF(usuario.getCpf());
 		usuarioService.save(usuario);
 		emailServico.enviarEmail();
-		return "redirect:/cadastro";
-	}
-	private static final int[] pesoCPF = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-	private static int calcularDigito(String str, int[] peso) {
-		int soma = 0;
-		for (int indice = str.length() - 1, digito; indice >= 0; indice--) {
-			digito = Integer.parseInt(str.substring(indice, indice + 1));
-			soma += digito * peso[peso.length - str.length() + indice];
-		}
-		soma = 11 - soma % 11;
-		return soma > 9 ? 0 : soma;
-	}
-
-	public static boolean isValidCPF(String cpf) {
-		if ((cpf == null) || (cpf.length() != 11))
-			return false;
-
-		Integer digito1 = calcularDigito(cpf.substring(0, 9), pesoCPF);
-		Integer digito2 = calcularDigito(cpf.substring(0, 9) + digito1, pesoCPF);
-		return cpf.equals(cpf.substring(0, 9) + digito1.toString()
-				+ digito2.toString());
-		
-}
-	 public static boolean validarEmail(String email)
-	    {
-	        boolean isEmailIdValid = false;
-	        if (email != null && email.length() > 0) {
-	            String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-	            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-	            Matcher matcher = pattern.matcher(email);
-	            if (matcher.matches()) {
-	                isEmailIdValid = true;
-	            }
-	        }
-	        return isEmailIdValid;
-	    }
+		return "index";
 	
-
 	// @RequestMapping(value = "/", method = RequestMethod.GET)
 	// public String showUserForm(Model model) {
 	// model.addAttribute("usuario",new Usuario());
@@ -93,4 +64,5 @@ public class UsuarioController {
 	//
 	// }
 
+	}
 }
--- WebHomeBeta/src/main/webapp/WEB-INF/webdinamic/cadastro.jsp
+++ WebHomeBeta/src/main/webapp/WEB-INF/webdinamic/cadastro.jsp
@@ -1,6 +1,7 @@
 <%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <!DOCTYPE html>
 <html lang="pt_BR">
 
@@ -53,7 +54,7 @@
 						
 						<form:label for="dt_nascimento" path="dt_nascimento">Data de nascimento:</form:label>
 						<form:input path="dt_nascimento"  cssClass="${(list[4]) ? '' : 'error'}" />
-						
+						<!--<fmt:formatDate value="${dt_nascimento.time}" pattern="DD/MM/YYYY"/>-->
 						<form:label class="lblRight" for="cpf" path="cpf">CPF: *</form:label>
 						<form:input path="cpf" cssClass="${(list[5]) ? '' : 'error'}" />
 						
