8ab733ca595eb1e6910ccc6226fd47b869573346
Fernandolima <fernandolima66@gmail.com>
2013-09-18 15:47:59 -0700

    Oi

---
+++ WebHomeBeta/src/main/java/br/com/webhomebeta/bean/UploadArquivosControllerBean.java
@@ -0,0 +1,57 @@
+package br.com.webhomebeta.bean;
+
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import br.com.webhomebeta.entity.Usuario;
+
+public class UploadArquivosControllerBean {
+	
+private Usuario usuario;
+	
+	private String fileName;
+	
+	private CommonsMultipartFile fileData;
+
+	/**
+	 * @return the usuario
+	 */
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @return the fileData
+	 */
+	public CommonsMultipartFile getFileData() {
+		return fileData;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @param fileName the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	 * @param fileData the fileData to set
+	 */
+	public void setFileData(CommonsMultipartFile fileData) {
+		this.fileData = fileData;
+	}
+
+}
